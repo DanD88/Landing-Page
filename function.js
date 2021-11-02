1495c1d26670d22227edd4fa447910bc6c91af7d
Daniel Davidson <DanDavidson88@gmail.com>
2021-11-02 14:46:16 -0700

    init files

---
+++ function.js
@@ -0,0 +1,36 @@
+// navigation icon
+
+function myFunction() {
+  var x = document.getElementById("nav-bar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+window.onscroll = function () { myFunction2() };
+
+// var topnav = document.getElementById("topnav");
+// var sticky = topnav.offsetTop;
+
+function myFunction2() {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
+
+
+// Pop up button 
+
+$('button').click(function(){
+  $('.popup').addClass('open');
+});
+ 
+$('.popup .closebtn').click(function(){
+  $('.popup').removeClass('open');
+});
+
